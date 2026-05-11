78e9e9fc30fa8a4b061c8b1217fe9fcebf5b62d0
Md Abdul Ahad <abd.me7@gmail.com>
2026-05-11 14:35:39 +0600

    python variable

--- Python/app.py
+++ Python/app.py
@@ -7,15 +7,3 @@ rating = 4.9 #
 is_published = False # Boolean. 
 course_name = "Python Programming"
 print(student_count)
-
-
-
-course = "Python \"Programming\" \\ \'Coding\'\n"
-print(course)
-
-# Escape Syntax
-
-# \' will print: '
-# \" will print: "
-# \\ will print: \
-# \n will print: new line
