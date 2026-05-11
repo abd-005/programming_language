fa4e341ba71ae669e43408c46c4c8b8b13010368
Md Abdul Ahad <abd.me7@gmail.com>
2026-05-11 14:50:14 +0600

    python escape syntax

--- Python/app.py
+++ Python/app.py
@@ -6,16 +6,28 @@ student_count = 1000 # Integer
 rating = 4.9 #
 is_published = False # Boolean. 
 course_name = "Python Programming"
-print(student_count)
+# print(student_count)
 
 
 
 # ---String---
 course = "Python Programming"
-print(len(course)) # print: length of the String
-print(course[0]) # print: 1st word of the String
-print(course[-1]) # print: last word of the String
-print(course[0:3]) # print: first 4 words of the String
-print(course[0:]) # print: till the last word of the String
-print(course[:3]) # print: first 4 words of the String
-print(course[:]) # print: whole String
+# print(len(course)) # print: length of the String
+# print(course[0]) # print: 1st word of the String
+# print(course[-1]) # print: last word of the String
+# print(course[0:3]) # print: first 4 words of the String
+# print(course[0:]) # print: till the last word of the String
+# print(course[:3]) # print: first 4 words of the String
+# print(course[:]) # print: whole String
+
+# ---Escape Syntax---
+
+
+course = "Python \"Programming\" \\ \'Coding\'\n"
+print(course)
+
+
+# \' will print: '
+# \" will print: "
+# \\ will print: \
+# \n will print: new line
