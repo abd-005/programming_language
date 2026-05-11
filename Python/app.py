0981d273ba715a583598aaedff0be2b1cd0ca57c
Md Abdul Ahad <abd.me7@gmail.com>
2026-05-11 14:32:33 +0600

    python variable

---
+++ Python/app.py
@@ -0,0 +1,21 @@
+# Variables
+
+# python is case sensitive
+
+student_count = 1000 # Integer
+rating = 4.9 #
+is_published = False # Boolean. 
+course_name = "Python Programming"
+print(student_count)
+
+
+
+course = "Python \"Programming\" \\ \'Coding\'\n"
+print(course)
+
+# Escape Syntax
+
+# \' will print: '
+# \" will print: "
+# \\ will print: \
+# \n will print: new line
