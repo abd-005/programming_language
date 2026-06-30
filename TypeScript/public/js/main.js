bc10baaad3523f977f935d971e152713c42a8319
Md Abdul Ahad <abd.me7@gmail.com>
2026-06-30 23:42:48 +0600

    add: Objects in TS

--- TypeScript/public/js/main.js
+++ TypeScript/public/js/main.js
@@ -3,46 +3,71 @@
 //    string number any boolean union       
 ////////////////////////////////////////////////////////////////////
 Object.defineProperty(exports, "__esModule", { value: true });
-let devs; //Union
-let likely_JS;
-let num;
-console.log();
+// let devs: string | number | boolean; //Union
+// let likely_JS: any;
+// let num: number;
+// console.log()
 ////////////////////////////////////////////////////////////////////
 //    Function       
 ////////////////////////////////////////////////////////////////////
 // Function that returns String 
-const ageCalString = (name, age) => {
-    return `Hi ${name},  your age is: ${age}`;
-};
-console.log(ageCalString("Fradin", 30));
+// const ageCalString = (name: string, age: number): string => {
+//     return `Hi ${name},  your age is: ${age}`;
+// }
+// console.log(ageCalString("Fradin", 30))
 // Function that returns Void | which doesn't return anything & can run operations like log
-const ageCalVoid = (name, age) => {
-    console.log(`Hi ${name}, you're ${age} years old!`);
-};
-ageCalVoid("Nadim", 20);
+// const ageCalVoid = (name: string, age: number): void => {
+//     console.log(`Hi ${name}, you're ${age} years old!`)
+// }
+// ageCalVoid("Nadim", 20)
 // Function that never returns | which never return anything & can throw error
-const info = (name) => {
-    throw new Error("");
-};
+// const info = (name: string): never => {
+//     throw new Error("");
+// }
 // info("Hakim")
 ////////////////////////////////////////////////////////////////////
 //   Array       
 ////////////////////////////////////////////////////////////////////
 // String
-const food = ["Mango", "Apple", "Watermelon"];
+// const food: string[] = ["Mango", "Apple", "Watermelon"] 
 // Union
-const foodData = ["Mango", "Apple", "Watermelon", 13, 87, false];
-foodData.push(true);
-foodData.push(32);
-foodData.push("Banana");
-console.log(foodData);
+// const foodData: (string | number | boolean)[] = ["Mango", "Apple", "Watermelon", 13, 87, false]
+// foodData.push(true)
+// foodData.push(32)
+// foodData.push("Banana")
+// console.log(foodData)
 // Tuples
-const developers = ["Hamood", 23, true];
-developers[0] = "Harris";
-// developers[1] = "Twenty"; // will throw error
-developers[1] = 20;
-developers[2] = false;
-developers.pop();
-developers.push("Pushed Data");
-console.log(developers);
+// const developers: [string, number, boolean] = ["Hamood", 23, true];
+// developers[0] = "Harris";
+// // developers[1] = "Twenty"; // will throw error
+// developers[1] = 20; 
+// developers[2] = false;
+// developers.pop()
+// developers.push("Pushed Data")
+// console.log(developers)
+////////////////////////////////////////////////////////////////////
+//   Array       
+////////////////////////////////////////////////////////////////////
+// Implicit type assign
+const devs = {
+    name: "Harris",
+    age: 23,
+    skill: true,
+};
+// Implicit type assign X optional chaining X readonly
+const developers = {
+    name: "Hamood",
+    age: 28,
+    skill: true,
+};
+// const users: User = {
+//     name: "Harris",
+//     age: 23,
+//     skill: true,
+// } 
+const users = {
+    name: "Zareef",
+    age: 23,
+    skill: true,
+};
 //# sourceMappingURL=main.js.map
--- TypeScript/src/main.ts
+++ TypeScript/src/main.ts
@@ -4,11 +4,11 @@
 
  
 
-let devs: string | number | boolean; //Union
-let likely_JS: any;
-let num: number;
+// let devs: string | number | boolean; //Union
+// let likely_JS: any;
+// let num: number;
 
-console.log()
+// console.log()
 
 
 ////////////////////////////////////////////////////////////////////
@@ -18,27 +18,27 @@ console.log()
 
 // Function that returns String 
 
-const ageCalString = (name: string, age: number): string => {
-    return `Hi ${name},  your age is: ${age}`;
-}
+// const ageCalString = (name: string, age: number): string => {
+//     return `Hi ${name},  your age is: ${age}`;
+// }
 
-console.log(ageCalString("Fradin", 30))
+// console.log(ageCalString("Fradin", 30))
 
 
 // Function that returns Void | which doesn't return anything & can run operations like log
 
-const ageCalVoid = (name: string, age: number): void => {
-    console.log(`Hi ${name}, you're ${age} years old!`)
-}
+// const ageCalVoid = (name: string, age: number): void => {
+//     console.log(`Hi ${name}, you're ${age} years old!`)
+// }
 
-ageCalVoid("Nadim", 20)
+// ageCalVoid("Nadim", 20)
 
 
 // Function that never returns | which never return anything & can throw error
 
-const info = (name: string): never => {
-    throw new Error("");
-}
+// const info = (name: string): never => {
+//     throw new Error("");
+// }
 
 // info("Hakim")
 
@@ -50,30 +50,85 @@ const info = (name: string): never => {
 
 // String
 
-const food: string[] = ["Mango", "Apple", "Watermelon"] 
+// const food: string[] = ["Mango", "Apple", "Watermelon"] 
 
 
 // Union
 
-const foodData: (string | number | boolean)[] = ["Mango", "Apple", "Watermelon", 13, 87, false]
+// const foodData: (string | number | boolean)[] = ["Mango", "Apple", "Watermelon", 13, 87, false]
 
-foodData.push(true)
-foodData.push(32)
-foodData.push("Banana")
+// foodData.push(true)
+// foodData.push(32)
+// foodData.push("Banana")
 
-console.log(foodData)
+// console.log(foodData)
 
 
 // Tuples
 
-const developers: [string, number, boolean] = ["Hamood", 23, true];
+// const developers: [string, number, boolean] = ["Hamood", 23, true];
 
-developers[0] = "Harris";
-// developers[1] = "Twenty"; // will throw error
-developers[1] = 20; 
-developers[2] = false;
+// developers[0] = "Harris";
+// // developers[1] = "Twenty"; // will throw error
+// developers[1] = 20; 
+// developers[2] = false;
 
-developers.pop()
-developers.push("Pushed Data")
+// developers.pop()
+// developers.push("Pushed Data")
 
-console.log(developers)
+// console.log(developers)
+
+
+////////////////////////////////////////////////////////////////////
+        //   Array       
+////////////////////////////////////////////////////////////////////
+
+
+// Implicit type assign
+
+const devs: {} = {
+    name: "Harris",
+    age: 23,
+    skill: true,
+} 
+
+
+// Implicit type assign X optional chaining X readonly
+
+const developers: { readonly name: string, age: number, skill?: boolean } = {
+    name: "Hamood",
+    age: 28,
+    skill: true,
+} 
+
+
+// Optimized way
+
+type User = {
+    readonly name: string;
+    age: number;
+    skill?: boolean;
+}
+
+
+// Other way of type
+interface Persons {
+    readonly name: string;
+    age: number;
+    skill?: boolean;
+}
+
+
+// const users: User = {
+//     name: "Harris",
+//     age: 23,
+//     skill: true,
+// } 
+
+const users: Persons = {
+    name: "Zareef",
+    age: 23,
+    skill: true,
+} 
+
+
